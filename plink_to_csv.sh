f8d051bfd30f9c592f1d8db26b1f737a359897e1
Marc Teale <marcteale@users.noreply.github.com>
2017-03-23 14:42:33 -0700

    Initial commit.

---
+++ plink_to_csv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "Too few arguments."
+  echo "Usage: $0 outfile plinkfile1 .. plinkfileN"
+  exit
+fi
+
+outfile=$1
+
+for infile in ${BASH_ARGV[*]}; do
+  if [ $infile != $outfile ]; then
+    header=$(head -n1 $infile)
+    lines=$(expr $(wc -l $infile | awk '{print $1}') - 1)
+    tail -n ${lines} $infile >> $outfile
+  fi
+done
+
+if [ "${header: -1}" == " " ]; then
+  len=$(expr ${#header} - 1)
+  header=$(expr substr "$header" 1 $len)
+fi
+
+sed -i 1i\\"$header" $outfile
+perl -p -i -e 's/^\s+//' $outfile
+perl -p -i -e 's/ +/,/g' $outfile
+perl -p -i -e 's/\n/,\n/g' $outfile
